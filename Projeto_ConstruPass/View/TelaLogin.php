cd31e330381a4f9ebf3a3aa0ef6f291005aafadc
Matheus <matheus251170@outlook.com>
2021-01-18 07:19:43 -0300

    update

--- Projeto_ConstruPass/View/TelaLogin.php
+++ Projeto_ConstruPass/View/TelaLogin.php
@@ -8,39 +8,34 @@
     <title>Login</title>
 </head>
 <body>
-<style>
 
-      
-        
-</style>
-
-<div>
-  
-  <form class="borda" action="login.php" method="POST">
-    <div class="title">
-      <h2 style="font-family: 'Montserrat', sans-serif; font-size: 35px;">Log in</h2>
-      <h4 style="font-family: 'Montserrat', sans-serif; font-size: 20px; color: grey;">Insira seu Log in e a senha</h4>
-      <br>
-    </div>
-
-    <div class="contai">
-      <input style="border-radius: 6px; width:350px; margin-bottom: auto;" type="text" placeholder="E-mail" name="email" required>
-      <input style="border-radius: 6px; width:350px" type="password" placeholder="Senha" name="psw" required>
-    </div>
+  <div>
     
-    <div class="form">
-        
-      <button class="btm" type="submit">Login</button>
-      <span style="font-size: 12px;">Esqueceu sua <a style="font-size: 12px;" href="??">senha</a>?</span>
+    <form class="borda" action="login.php" method="POST">
+      <div class="title">
+        <h2 style="font-family: 'Montserrat', sans-serif; font-size: 35px;">Log in</h2>
+        <h4 style="font-family: 'Montserrat', sans-serif; font-size: 20px; color: grey;">Insira seu Log in e a senha</h4>
         <br>
-    </div>
+      </div>
 
-    <div class="under" style="background-color:#252222">
-        <p style="color: #ffffff;">Ainda não tem conta?</p>
-        <span><a style="color: rgb(230, 149, 0);" href="TelaCadastrodeCliente.php" rel = "next">Cadastre-se</a></span>
-    </div>
-  </form>
-</div>
+      <div class="contai">
+        <input style="border-radius: 6px; width:350px; margin-bottom: auto;" type="text" placeholder="E-mail" name="email" required>
+        <input style="border-radius: 6px; width:350px" type="password" placeholder="Senha" name="psw" required>
+      </div>
+      
+      <div class="form">
+          
+        <button class="btm" type="submit">Login</button>
+        <span style="font-size: 12px;">Esqueceu sua <a style="font-size: 12px;" href="??">senha</a>?</span>
+          <br>
+      </div>
+
+      <div class="under" style="background-color:#252222">
+          <p style="color: #ffffff;">Ainda não tem conta?</p>
+          <span><a style="color: rgb(230, 149, 0);" href="TelaCadastrodeCliente.php" rel = "next">Cadastre-se</a></span>
+      </div>
+    </form>
+  </div>
 
 
 </body>
